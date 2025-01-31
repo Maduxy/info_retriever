b2090c60abfcb232a506ae78a310ab7dcfb269d7
Madu <onyejiakamadu@gmail.com>
2025-01-31 10:07:24 +0100

    updated time format

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ app.add_middleware(
 @app.get("/profile")
 def my_info():
     return {"email": "onyejiakamadu@gmail.com",
-            "current_datetime":datetime.now(pytz.utc).isoformat() + "Z" ,
+            "current_datetime":datetime.now(pytz.utc).strftime("%Y-%m-%dT%H:%M:%SZ") ,
             "github_url": "https://github.com/Maduxy/info_retriever"}
 
